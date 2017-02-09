a06d70b9ae292246b9a18f5dbf6ae27d6566c24d
Dino Kraljeta <Dino Kraljeta>
2017-02-09 23:11:04 +0100

    different pics and stuff

--- client/main.js
+++ client/main.js
@@ -4,27 +4,53 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Meteor.subscribe("theCandybar");
+Meteor.subscribe("theCandybox");
 
 
 Template.links.rendered = function () {
-  $('.col-xs-12').animate({opacity:1, top:"0px"}, 1000);
+  $('.col-xs-12').animate({
+    opacity:1, 
+    top:"0px"
+  }, 1000);
 };
 
 Template.links.helpers({
   candybar: function(){
     return Candybar.find();
+  },
+  candyplace:function(){
+    return Candybox.findOne().candyplace;
   }
 })
 
 Template.links.events({
   'click .candybox-lid':function(event){
     $('.candybox-lid').addClass('open');
-    $('.candybox-lid').animate({top:'-600px', left:'600px'})
-    $('.candy').animate({opacity:1}, 600);
+    $('.candybox-lid').animate({
+      top:'-600px', 
+      left:'600px',
+      })
+    $('.candy').animate({
+      opacity:1
+    }, 600);
+  },
+  'mouseenter .candybox-lid':function(event){
+    $('.candybox-lid').animate({
+      opacity:'0.9'
+      }, 60);
+  },
+  'mouseout .candybox-lid':function(event){
+    $('.candybox-lid').animate({
+      opacity:'1'
+      }, 60);
   },
   'click .candybox-lid.open':function(event){
     $('.candybox-lid').removeClass('open');
-    $('.candybox-lid').animate({top:'0px', left:'15px'})
+    $('.candy').addClass('wiggly');
+    $('.candybox-lid').animate({
+      top:'0px',
+      left:'15px'
+    });
     $('.candy').animate({opacity:0}, 600);
     setTimeout(function(){
       for (i = 0; i <= 11; i++){
@@ -35,12 +61,12 @@ Template.links.events({
   'click #pictures':function(event){
     $('#pictures').modal('hide');
   },
-  'click .candy':function(event){
+  'click .wiggly':function(event){
     var candy = this._id;
     var img = Candybar.findOne(candy).img;
     var audio = Candybar.findOne(candy).audioId;
-
-    $(".picture").attr('src',img);
+    
+    $("#"+candy).removeClass('wiggly');
     $("#"+candy).fadeOut(function() { 
       $(this).load(function() { $(this).fadeIn(); }); 
       $("#"+candy).addClass('thumb');
@@ -49,5 +75,22 @@ Template.links.events({
 
     var clip = document.getElementById(audio);
     clip.play();
-  }
+  },
+  'click .candy':function(){
+    var candy = this._id;
+    var img = Candybar.findOne(candy).img;
+    $(".picture").attr('src',img);
+  },
+  'mouseover .candy':function(event){
+    var candyId = this._id;
+    $("#"+candyId).animate({
+      opacity: 0.6
+    }, 100);
+  },
+  'mouseout .candy':function(event){
+    var candyId = this._id;
+    $("#"+candyId).animate({
+      opacity: 1
+    }, 40);
+  },
 });
--- server/main.js
+++ server/main.js
@@ -15,10 +15,22 @@ Meteor.startup(() => {
             );
         }
     }
-   console.log("candies added: " +Candybar.find().count());
+  if (Candybox.find().count() == 0){
+        for (var i = 0 ; i <= 11 ; i++ ){
+        Candybox.insert(
+            { 
+            candyplace:"c" + i
+            }
+            );
+        }
+    }
+   console.log("candies in the box: " +Candybar.find().count());
 });
 
 
 Meteor.publish("theCandybar", function(){
 	return Candybar.find();
+})
+Meteor.publish("theCandybox", function(){
+	return Candybox.find();
 })
